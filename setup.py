8499c0c4f67543bce3fb2784b02e815cc100b262
ScubbX <markusmayr@gmx.net>
2015-06-15 17:43:51 +0200

    repaired

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='mpcouch',
-      version='0.1',
+      version='0.2',
       description='A multiprocess bulk-uploading helper for CouchDB',
       url='https://github.com/scubbx/couch-bulk-multiprocess',
       author='Markus Mayr',
