fb05da4a30469422d5bf4b2e65d0790ca5e19cc8
ScubbX <markusmayr@gmx.net>
2015-06-18 16:04:37 +0200

    some tests with threading instead of processes

---
+++ tests/test_mpcouch.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+
+import unittest
+import mpcouch
+
+
+class mpcouchTest(unittest.TestCase):
+
+    def test_pushData(self):
+        myc = mpcouch.mpcouchPusher("http://127.0.0.1:5984/testdb",100)
+        self.assertIsInstance(myc,mpcouch.mpcouchPusher)
+        
+        # for 1 through 100, all entries should be collected
+        for i in range(1,100): self.assertEqual(myc.pushData({"id":i}),i)
+        
+        # the 101st should start an upload and reset the collection:
+        self.assertEqual(myc.pushData({"id":101}),0)
+        # this one counts form one onwards again:
+        #self.assertEqual(myc.pushData({"id":102}),1)
+        
+        for i in range(102,152): self.assertEqual(myc.pushData({"id":i}),i-101)
+        
+        self.assertEqual(myc.finish(),50)
+        
+        self.assertEqual(myc.destroyDatabase(),None)
+        del myc
+    
+    def test_pushData_highLoad(self):
+        myc = mpcouch.mpcouchPusher("http://127.0.0.1:5984/testdb",10000)
+        
+        for usteps in range(2):
+            for i in range(1,10000): self.assertEqual(myc.pushData({"id":str(usteps) + str(i)}),i)
+            self.assertEqual(myc.pushData({"id":str(usteps) + str(10001)}),0)
+        
+        self.assertEqual(myc.finish(),0)
+        
+        self.assertEqual(myc.destroyDatabase(),None)
+        del myc
+
+unittest.main()
